42e72cd29413469331edfecd1b795ad109f3566e
Michał Kruszewski <mkru@protonmail.com>
2022-07-31 09:12:09 +0200

    Add function converting bitv_vector to slv_vector.

--- types.vhd
+++ types.vhd
@@ -14,16 +14,29 @@ package types is
    type bitv_vector_2d is array (natural range <>) of bitv_vector;
    type bitv_vector_3d is array (natural range <>) of bitv_vector_2d;
 
-
    type slv_vector    is array (natural range <>) of std_logic_vector;
    type slv_vector_2d is array (natural range <>) of slv_vector;
    type slv_vector_3d is array (natural range <>) of slv_vector_2d;
 
+   function to_slv_vector(bv : bitv_vector) return slv_vector;
 
    subtype int64        is signed(63 downto 0);
    type    int64_vector is array (natural range <>) of int64;
 
-
    type string_vector is array (natural range <>) of string;
 
 end package;
+
+
+package body types is
+
+   function to_slv_vector(bv : bitv_vector) return slv_vector is
+      variable slvv : slv_vector(bv'range)(bv(0)'range);
+   begin
+      for i in bv'range loop
+         slvv(i) := to_std_logic_vector(bv(i));
+      end loop;
+      return slvv;
+   end function;
+
+end package body;
