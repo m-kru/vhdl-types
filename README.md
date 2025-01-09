b8dda92ab11791096190f389b78b85f0a7ba12d6
Michał Kruszewski <mkru@protonmail.com>
2025-01-09 12:25:07 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# vhdl-types
+
+VHDL library collecting useful types that are frequently used, but are not part of the standard.
+
+Files:
+- _types.vhd_ - vhdl types package,
+- _types.hbs_ - library definition for the [HBS](https://github.com/m-kru/hbs) built system,
+- _types.core_ - library definition for the [FuseSoc](https://github.com/olofk/fusesoc) built system.
--- types.vhd
+++ types.vhd
@@ -3,40 +3,42 @@
 -- Copyright (c) 2022 Michał Kruszewski
 
 library ieee;
-   use ieee.std_logic_1164.all;
-   use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 
--- Package types collects useful types that are frequently used, but are not part of the standard.
+-- Collection of useful types that are frequently used, but are not part of the standard.
 package types is
 
-   type bitv_vector    is array (natural range <>) of bit_vector;
-   type bitv_vector_2d is array (natural range <>) of bitv_vector;
-   type bitv_vector_3d is array (natural range <>) of bitv_vector_2d;
+  type bitv_vector    is array (natural range <>) of bit_vector;
+  type bitv_vector_2d is array (natural range <>) of bitv_vector;
+  type bitv_vector_3d is array (natural range <>) of bitv_vector_2d;
 
-   type slv_vector    is array (natural range <>) of std_logic_vector;
-   type slv_vector_2d is array (natural range <>) of slv_vector;
-   type slv_vector_3d is array (natural range <>) of slv_vector_2d;
+  type slv_vector    is array (natural range <>) of std_logic_vector;
+  type slv_vector_2d is array (natural range <>) of slv_vector;
+  type slv_vector_3d is array (natural range <>) of slv_vector_2d;
 
-   function to_slv_vector(bv : bitv_vector) return slv_vector;
+  -- Converts bitv_vector to slv_vector.
+  function to_slv_vector(bv : bitv_vector) return slv_vector;
 
-   subtype int64        is signed(63 downto 0);
-   type    int64_vector is array (natural range <>) of int64;
+  -- NOTE: VHDL 2019 expanded the built-in integer type to 64 bits.
+  subtype int64        is signed(63 downto 0);
+  type    int64_vector is array (natural range <>) of int64;
 
-   type string_vector is array (natural range <>) of string;
+  type string_vector is array (natural range <>) of string;
 
 end package;
 
 
 package body types is
 
-   function to_slv_vector(bv : bitv_vector) return slv_vector is
-      variable slvv : slv_vector(bv'range)(bv(0)'range);
-   begin
-      for i in bv'range loop
-         slvv(i) := to_std_logic_vector(bv(i));
-      end loop;
-      return slvv;
-   end function;
+  function to_slv_vector(bv : bitv_vector) return slv_vector is
+    variable slvv : slv_vector(bv'range)(bv(0)'range);
+  begin
+    for i in bv'range loop
+      slvv(i) := to_std_logic_vector(bv(i));
+    end loop;
+    return slvv;
+  end function;
 
 end package body;
